ad08352d4dbc7feb3004907e131e8b91e587985f
Mayara Gouveia <gb8may@gmail.com>
2019-07-14 19:57:12 -0300

    Add script RG

---
+++ rg-create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Author: Mayara Gouveia
+# Contact: gb8may@gmail.com
+
+
+read -p "What name should i use in your ResourceGroup?" ResourceGroup
+ResourceGroup=$RG
+echo 
+read -p "In what Location the resource group will be hosted" LO
+LO=$LO
+echo
+echo "Creating your ResourceGroup..."
+echo 
+az group create --name $RG --location $LO
