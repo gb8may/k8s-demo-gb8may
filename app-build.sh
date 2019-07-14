b3510399226cec0796aa3237a22ea6a0a8478029
Mayara Gouveia <gb8may@gmail.com>
2019-07-14 20:21:25 -0300

    add ingress

--- app-build.sh
+++ app-build.sh
@@ -13,11 +13,15 @@ az aks get-credentials --resource-group $RG --name $CL
 
 kubectl apply -f azure-vote.yaml
 
+kubectl apply -f ingress.yaml
+
 echo "Creating a service account for Tiller service... Don't worry, i'll do it for you!"
 
 kubectl apply -f helm-rbac.yaml
 
 helm init --service-account tiller --node-selectors "beta.kubernetes.io/os"="linux"
+
+helm repo update
 
 echo "Installing Nginx-Ingress..."
 
