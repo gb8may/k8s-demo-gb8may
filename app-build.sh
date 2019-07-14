2d69f07fe82919ee5fbff847ae06d69dc385ab37
Mayara Gouveia <gb8may@gmail.com>
2019-07-14 19:47:53 -0300

    going on the script

--- app-build.sh
+++ app-build.sh
@@ -10,3 +10,12 @@ kubectl apply -f helm-rbac.yaml
 
 helm init --service-account tiller --node-selectors "beta.kubernetes.io/os"="linux"
 
+echo "Installing Nginx-Ingress..."
+
+kubectl create namespace ingress-basic
+
+helm install stable/nginx-ingress \
+    --namespace ingress-basic \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
