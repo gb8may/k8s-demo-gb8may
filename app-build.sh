7cbe62286867c4be30559aed338782d099f84b16
Mayara Gouveia <gb8may@gmail.com>
2019-07-14 20:11:49 -0300

    script update

--- app-build.sh
+++ app-build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+read -p "What is your ResourceGroup name?" RG
+ResourceGroup=$RG
+echo 
+read -p "What is your AKS cluster name?" CL
+Cluster=$CL
+echo
+
 echo "Building app.."
+
+az aks get-credentials --resource-group $RG --name $CL
 
 kubectl apply -f azure-vote.yaml
 
