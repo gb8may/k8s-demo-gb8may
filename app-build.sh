a8df62154cf81f05ea820523855bc4ce471dfdac
Mayara Gouveia <gb8may@gmail.com>
2019-07-14 20:28:16 -0300

    teste

--- app-build.sh
+++ app-build.sh
@@ -17,18 +17,18 @@ kubectl apply -f ingress.yaml
 
 echo "Creating a service account for Tiller service... Don't worry, i'll do it for you!"
 
-kubectl apply -f helm-rbac.yaml
+#kubectl apply -f helm-rbac.yaml
 
-helm init --service-account tiller --node-selectors "beta.kubernetes.io/os"="linux"
+#helm init --service-account tiller --node-selectors "beta.kubernetes.io/os"="linux"
 
-helm repo update
+#helm repo update
 
-echo "Installing Nginx-Ingress..."
+#echo "Installing Nginx-Ingress..."
 
-kubectl create namespace ingress-basic
+#kubectl create namespace ingress-basic
 
-helm install stable/nginx-ingress \
-    --namespace ingress-basic \
-    --set controller.replicaCount=2 \
-    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
-    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
+#helm install stable/nginx-ingress \
+#    --namespace ingress-basic \
+#    --set controller.replicaCount=2 \
+#    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+#    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
