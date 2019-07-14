b8070beb21bdd1649cf81844091a9eeca665e511
Mayara Gouveia <gb8may@gmail.com>
2019-07-14 19:45:47 -0300

    going on the script

--- app-build.sh
+++ app-build.sh
@@ -4,4 +4,9 @@ echo "Building app.."
 
 kubectl apply -f azure-vote.yaml
 
+echo "Creating a service account for Tiller service... Don't worry, i'll do it for you!"
+
+kubectl apply -f helm-rbac.yaml
+
+helm init --service-account tiller --node-selectors "beta.kubernetes.io/os"="linux"
 
