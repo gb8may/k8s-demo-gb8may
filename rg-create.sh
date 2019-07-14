5d524a977c002abc8835f1fbfc7b222ce2e4a0b9
Mayara Gouveia <gb8may@gmail.com>
2019-07-14 19:59:34 -0300

    script update

--- rg-create.sh
+++ rg-create.sh
@@ -4,8 +4,8 @@
 # Contact: gb8may@gmail.com
 
 
-read -p "What name should i use in your ResourceGroup?" ResourceGroup
-ResourceGroup=$RG
+read -p "What name should i use in your ResourceGroup?" RG
+RG=$RG
 echo 
 read -p "In what Location the resource group will be hosted" LO
 LO=$LO
