7cc07166382908719891ea9baa1473c7f9dc8e83
Mayara Gouveia <gb8may@gmail.com>
2019-07-14 20:45:27 -0300

    update script

--- app-build.sh
+++ app-build.sh
@@ -13,22 +13,19 @@ az aks get-credentials --resource-group $RG --name $CL
 
 kubectl apply -f azure-vote.yaml
 
-kubectl apply -f ingress.yaml
-
 echo "Creating a service account for Tiller service... Don't worry, i'll do it for you!"
 
-#kubectl apply -f helm-rbac.yaml
+kubectl apply -f helm-rbac.yaml
 
-#helm init --service-account tiller --node-selectors "beta.kubernetes.io/os"="linux"
+helm init --service-account tiller --node-selectors "beta.kubernetes.io/os"="linux"
 
-#helm repo update
+helm repo update
 
-#echo "Installing Nginx-Ingress..."
+echo "Installing Nginx-Ingress..."
 
-#kubectl create namespace ingress-basic
+helm install stable/nginx-ingress \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
 
-#helm install stable/nginx-ingress \
-#    --namespace ingress-basic \
-#    --set controller.replicaCount=2 \
-#    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
-#    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
+kubectl apply -f ingress.yaml
+
