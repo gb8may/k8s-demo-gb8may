4d106f6787662962bd1d402a81f25e1a206b3d96
Mayara Gouveia <35708820+gb8may@users.noreply.github.com>
2019-07-14 21:20:46 -0300

    Update app-build.sh

--- app-build.sh
+++ app-build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+###########################################
+#         Autor: Mayara Gouveia           #
+#           gb8may@gmail.com              #
+#              Release 1.0                #
+###########################################
+
+
 read -p "What is your ResourceGroup name?" RG
 ResourceGroup=$RG
 echo 
